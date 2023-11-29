e4c20b6e718a9e33f9c692fabb43767a70ae04a7
Connor G <cpg200001@utdallas.edu>
2023-11-29 11:54:19 -0600

    Various Fixes
    Readjusted Spawn Location in AR Space, readjusted kill bounds, added "lose" functionality to some obstacles, fixed small start screen issue where the title didn't scale to the phone screen.

--- Assets/wipeout new/Gameplay Scripts/LoseGame.cs
+++ Assets/wipeout new/Gameplay Scripts/LoseGame.cs
@@ -13,4 +13,11 @@ public class LoseGame : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
 }
